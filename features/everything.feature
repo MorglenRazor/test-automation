ab60238e10a0cb39414370a58081d5ba54effd92
MorglenRazor <r.gubaev@bk.ru>
2021-05-16 15:24:28 +0300

    Create new steps 'find href' and 'Checking URL'

--- features/everything.feature
+++ features/everything.feature
@@ -5,3 +5,6 @@ Scenario: Сheck some text in search results
 #И используем наши шаги.
   Given website "https://yandex.ru/"
   Then insert text in search 'расчет расстояний между городами'
+  Then push button with text 'Найти'
+  Then find href avtodispetcher 'https://www.avtodispetcher.ru/distance/'
+  Then Checking our URl 'https://www.avtodispetcher.ru/distance/'
--- features/steps/avtodispetcher_test.py
+++ features/steps/avtodispetcher_test.py
@@ -1,3 +1,5 @@
+import time
+
 from behave import *
 
 from selenium import webdriver
@@ -18,14 +20,41 @@ def step(context, url):
 @then("insert text in search '{text}'")
 def step(context, text):
     input = WebDriverWait(context.browser, 120).until(
-        EC.element_to_be_clickable((By.XPATH, '//input[contains(@class, "input__control input__input mini-suggest__input")]'))
+        EC.element_to_be_clickable(
+            (By.XPATH, '//input[contains(@class, "input__control input__input mini-suggest__input")]'))
     )
     input.send_keys(text)
 
-#Теперь нажмем на кнопку "Найти"
+
+# Теперь нажмем на кнопку "Найти"
 @then("push button with text '{text}'")
 def step(context, text):
     WebDriverWait(context.browser, 120).until(
         EC.element_to_be_clickable((By.XPATH, '//button'))
     )
     context.browser.find_element_by_xpath('//button').click()
+
+
+@then("find href avtodispetcher '{text}'")
+def step(context, text):
+    WebDriverWait(context.browser, 120).until(
+        EC.element_to_be_clickable((By.XPATH, '//a[contains(@href, "{}")]'.format(text)))
+    )
+    context.browser.find_element_by_xpath('//a[contains(@href, "{}")]'.format(text)).click()
+
+
+@then("Checking URl '{text}'")
+def step(context, text):
+    # Запоминаем id страницы
+    new_id_page = context.browser.window_handles
+    # Переходим на нее
+    context.browser.switch_to.window(new_id_page[1])
+    # Считываем текуший URL страницы
+    curr_url = context.browser.current_url
+    print(curr_url)
+    # Проверяем его с искомым https://www.avtodispetcher.ru/distance/
+    if curr_url == text:
+        assert True
+
+    else:
+        assert False

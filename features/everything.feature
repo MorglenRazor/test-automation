2b63ec84690e059c417ed775878464ba63bb667f
MorglenRazor <r.gubaev@bk.ru>
2021-05-17 11:46:58 +0300

    Fin

--- features/everything.feature
+++ features/everything.feature
@@ -1,22 +1,23 @@
-#Укажем что это за фича
-Feature: Checking search
-#Укажем имя сценария (в одной фиче может быть несколько)
-Scenario: Сheck some text in search results
-#И используем наши шаги.
-  Given website "https://yandex.ru/"
-  Then insert text in search 'расчет расстояний между городами'
-  Then push button with text 'Найти'
-  Then find href avtodispetcher 'https://www.avtodispetcher.ru/distance/'
-  Then Checking URl 'https://www.avtodispetcher.ru/distance/'
-  Then Input value from 'Тула'
-  Then Input value to 'Санкт-Петербург'
-  Then Set value gas '9'
-  Then Set value price on gas '46'
-  Then Click on button with value 'Рассчитать'
-  Then Checking total distance '897'
-  Then Checking total price '3726'
-  Then Change route 'Настроить маршрут'
-  Then Find field between city and input city 'Великий Новгород'
-  Then Wait 30 second and click btn 'Рассчитать'
-  Then Checking new total distance '966'
-  And Checking new total price '4002'
+@logging
+@capture
+Feature: Checking avtodispetcher
+
+  Scenario: Сheck the construction of the route on the site www.avtodispetcher.ru
+
+    Given Website "https://yandex.ru/"
+    Then Insert text in search 'расчет расстояний между городами'
+    Then Push button with text 'Найти'
+    Then Find href avtodispetcher 'https://www.avtodispetcher.ru/distance/'
+    Then Checking URl 'https://www.avtodispetcher.ru/distance/'
+    Then Input value from 'Тула'
+    Then Input value to 'Санкт-Петербург'
+    Then Set value gas '9'
+    Then Set value price on gas '46'
+    Then Click on button with value 'Рассчитать'
+    Then Checking total distance '897'
+    Then Checking total price '3726'
+    Then Change route 'Настроить маршрут'
+    Then Find field between city and input city 'Великий Новгород'
+    Then Wait 30 second and click btn 'Рассчитать'
+    Then Checking new total distance '966'
+    Then Checking new total price '4002'
--- features/steps/avtodispetcher_test.py
+++ features/steps/avtodispetcher_test.py
@@ -5,21 +5,23 @@ import logging
 from behave import *
 
 from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+driver = '/home/morglen/PycharmProjects/test_automation/chromedriver'
 
-@given('website "{url}"')
+@given('Website "{url}"')
 def step(context, url):
-    # Измените строку, для выполнения теста в другом браузере
-    context.browser = webdriver.Chrome()
+    context.browser = webdriver.Chrome(driver)
     context.browser.maximize_window()
-    context.browser.get("https://yandex.ru/")
+    context.browser.get(url)
 
 
 # Вставляем в строку поиска "расчет расстояний между городами"
-@then("insert text in search '{text}'")
+@then("Insert text in search '{text}'")
 def step(context, text):
     input = WebDriverWait(context.browser, 120).until(
         EC.element_to_be_clickable(
@@ -29,7 +31,7 @@ def step(context, text):
 
 
 # Теперь нажмем на кнопку "Найти"
-@then("push button with text '{text}'")
+@then("Push button with text '{text}'")
 def step(context, text):
     WebDriverWait(context.browser, 120).until(
         EC.element_to_be_clickable((By.XPATH, '//button'))
@@ -37,7 +39,7 @@ def step(context, text):
     context.browser.find_element_by_xpath('//button').click()
 
 
-@then("find href avtodispetcher '{text}'")
+@then("Find href avtodispetcher '{text}'")
 def step(context, text):
     WebDriverWait(context.browser, 120).until(
         EC.element_to_be_clickable((By.XPATH, '//a[contains(@href, "{}")]'.format(text)))
@@ -53,14 +55,8 @@ def step(context, text):
     context.browser.switch_to.window(new_id_page[1])
     # Считываем текуший URL страницы
     curr_url = context.browser.current_url
-    print(curr_url)
     # Проверяем его с искомым https://www.avtodispetcher.ru/distance/
-    if curr_url == text:
-        assert True
-        logging.info("Искомая страница совпалает с текущей")
-    else:
-        logging.info("Искомая страница не совпадает с текушей")
-        assert False
+    assert curr_url == text
 
 
 @then("Input value from '{text}'")
@@ -110,12 +106,7 @@ def step(context, text):
     total_distance = WebDriverWait(context.browser, 120).until(
         EC.presence_of_element_located((By.XPATH, '//span[@id="totalDistance"]'.format(text)))
     )
-    if total_distance.text == text:
-        assert True
-        logging.info("Искомая дистанция совпалает с текущей")
-    else:
-        logging.info("Искомая дистанция совпалает с текущей")
-        assert False
+    assert total_distance.text == text
 
 
 @then("Checking total price '{text}'")
@@ -124,12 +115,7 @@ def step(context, text):
         EC.presence_of_element_located((By.XPATH, '//*[contains(text(), "{}")]'.format(text)))
     )
     context.browser.implicitly_wait(10)
-    if re.search(text, total_price.text):
-        assert True
-        logging.info("Искомая цена совпалает с текущей")
-    else:
-        logging.info("Искомая цена совпалает с текущей")
-        assert False
+    assert re.search(text, total_price.text)
 
 
 @then("Change route '{text}'")
@@ -154,7 +140,11 @@ def step(context, text):
     btn = WebDriverWait(context.browser, 120).until(
         EC.element_to_be_clickable((By.XPATH, '//input[@value="{}"]'.format(text)))
     )
-    btn.click()
+    try:
+        btn.click()
+    except ElementClickInterceptedException as e:
+        context.browser.execute_script("arguments[0].scrollIntoView();", btn)
+        btn.click()
 
 
 @then("Checking new total distance '{text}'")
@@ -162,12 +152,7 @@ def step(context, text):
     total_distance = WebDriverWait(context.browser, 120).until(
         EC.presence_of_element_located((By.XPATH, '//span[@id="totalDistance"]'))
     )
-    if total_distance.text == text:
-        assert True
-        logging.info("Искомая дистанция совпалает с текущей")
-    else:
-        logging.info("Искомая дистанция совпалает с текущей")
-        assert False
+    assert total_distance.text == text
 
 
 @then("Checking new total price '{text}'")
@@ -176,9 +161,4 @@ def step(context, text):
         EC.presence_of_element_located((By.XPATH, '//*[contains(text(), "{}")]'.format(text)))
     )
     context.browser.implicitly_wait(10)
-    if re.search(text, total_price.text):
-        assert True
-        logging.info("Искомая цена совпалает с текущей")
-    else:
-        logging.info("Искомая цена совпалает с текущей")
-        assert False
+    assert re.search(text, total_price.text)
